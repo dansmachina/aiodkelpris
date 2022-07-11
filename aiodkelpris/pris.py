f5c5e2312993facebda4c0b9b05c91a59bdd4d18
Alejandro <8874974+dansmachina@users.noreply.github.com>
2022-07-11 13:20:15 +0200

    feat(logging): removes loguru for logging (#7)

--- aiodkelpris/pris.py
+++ aiodkelpris/pris.py
@@ -1,13 +1,15 @@
 import asyncio
 import json
+import logging
 from datetime import datetime, timedelta
 from typing import List
 
 import aiohttp
-from loguru import logger
 
 from aiodkelpris.core.const import BASE_URL, DATE_TIME_FORMAT, PRICE_AREA
 from aiodkelpris.core.models import Price
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class DKElPris:
@@ -21,20 +23,22 @@ class DKElPris:
         self.price_area = price_area
 
         if price_area not in PRICE_AREA:
-            logger.error(
-                "Unknown price area {}. Should be one of {}", price_area, PRICE_AREA
+            _LOGGER.error(
+                "Unknown price area %s. Should be one of %s", price_area, PRICE_AREA
             )
-            raise ValueError(f"Invalid price area: {price_area}")
+            raise ValueError(
+                f"Invalid price area: {price_area}. Should be one of {PRICE_AREA}"
+            )
 
     async def _api_get_prices(self, url: str) -> List[Price]:
         assert self._session is not None
-        logger.debug("Requesting: '{}'", url)
+        _LOGGER.debug("Requesting: '%s'", url)
         resp = await self._session.get(url)
         if resp.status < 400:
             data = await resp.json()
             return data["records"]
         else:
-            logger.error("Error {}", resp)
+            _LOGGER.error("Error {}", resp)
             raise Exception(f"Error {resp}")
 
     async def _retrieve_prices(self, start: str, end: str) -> List[Price]:
@@ -43,10 +47,10 @@ class DKElPris:
         try:
             return await self._api_get_prices(url)
         except asyncio.TimeoutError:
-            logger.warning("Timeout error requesting data from '{}'", url)
+            _LOGGER.warning("Timeout error requesting data from '%s'", url)
             raise Exception(f"Error requesting data from '{url}'")
         except aiohttp.ClientError:
-            logger.warning("Client error in '{}'", url)
+            _LOGGER.warning("Client error in '%s'", url)
             raise Exception(f"Error requesting data from '{url}'")
 
     async def get_current_price(self) -> Price:
@@ -54,6 +58,6 @@ class DKElPris:
         now_plus_one = now + timedelta(hours=1)
         start = datetime.strftime(now, DATE_TIME_FORMAT)
         end = datetime.strftime(now_plus_one, DATE_TIME_FORMAT)
-        logger.debug("Time frame requested: {} - {}", start, end)
+        _LOGGER.debug("Time frame requested: %s - %s", start, end)
         price = await self._retrieve_prices(start, end)
         return price[0]
